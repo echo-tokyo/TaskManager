042a47aaea0e03f79724144f685120d44d6c2bb3
sayasha <2two2two8eight@mail.ru>
2024-10-27 10:20:17 +0300

    cards done

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -38,21 +38,24 @@ const isIframeActivated = ref(false)
         <input @click="addTask(), isModalActivated = false" type="button" value="Добавить">
     </div>
     <div class="card">
-        <h3 class="card__title">{{ props.title }}</h3>
+        <div class="card-top">
 
-        <draggable     
-            
-        class="container-for-tasks"
-        :list="props.tasks"
-        group="people"
-        @change="log"
-        :itemKey="props.title" >
-            <template  #item="{ element: tasks }" tag="div">
-                <Task @click="(event) => onTaskClicked(event), isIframeActivated = true" :task="tasks"/>
-            </template>
-        </draggable>
-        <!-- <div :class="'container-for-tasks'">
-        </div> -->
+            <h3 class="card__title">{{ props.title }}</h3>
+    
+            <draggable     
+                
+            class="container-for-tasks"
+            :list="props.tasks"
+            group="people"
+            @change="log"
+            :itemKey="props.title" >
+                <template  #item="{ element: tasks }" tag="div">
+                    <Task @click="(event) => onTaskClicked(event), isIframeActivated = true" :task="tasks"/>
+                </template>
+            </draggable>
+            <!-- <div :class="'container-for-tasks'">
+            </div> -->
+        </div>
         <div class="add-task" @click="isModalActivated = true">
             <p>Добавить задачу</p>
         </div>
@@ -60,6 +63,21 @@ const isIframeActivated = ref(false)
 </template>
 
 <style>
+.card-top{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    overflow: scroll;
+}
+.card-top::-webkit-scrollbar {
+    /* position: absolute;
+    width: 3px;
+    height: 3px; */
+    background-color: #f4f3f3;
+    width: 0px;
+    height: 0px;
+}
 .iframe {
     border-radius: 20px;
     left: 15vw;
@@ -128,8 +146,10 @@ const isIframeActivated = ref(false)
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: 10px;
-    width: 85%;
+    /* margin-top: 10px; */
+    /* width: calc(100% - 15vw); */
+    width: calc(15vw + 40px);
+    margin-right: 5px;
     height: 50px;
     background-color: #ffffff;
     border: 1px solid #D9D9D9;
@@ -153,6 +173,7 @@ const isIframeActivated = ref(false)
 .container-for-tasks {
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
     gap: 8px;
     overflow: scroll;
     overflow-x: hidden;
@@ -161,29 +182,32 @@ const isIframeActivated = ref(false)
 }
 
 ::-webkit-scrollbar {
-    position: absolute;
+    /* position: absolute;
     width: 3px;
-    height: 3px;
-    background-color: #D9D9D9;
+    height: 3px; */
+    background-color: #f4f3f3;
+    width: 2px;
+    height: 2px;
 }
 
 ::-webkit-scrollbar-thumb {
     border-radius: 9em;
-    background-color: #474747;
+    background-color: rgb(214, 214, 214);
 }
 
-::-webkit-scrollbar-thumb:hover {
+/* ::-webkit-scrollbar-thumb:hover {
     background-color: #9e9e9e;
-}
+}  */
 
 .card {
-    padding-top: 20px;
-    padding-bottom: 20px;
+    gap: 10px;
+    padding-top: 50px;
+    padding-bottom: 50px;
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 22vw;
-    height: 75vh;
+    height: 65vh;
     justify-content: space-between;
     background-color: #fff;
     border-radius: 20px;
@@ -191,7 +215,7 @@ const isIframeActivated = ref(false)
 }
 
 .card__title {
-    margin-bottom: 10px;
+    margin-bottom: 20px;
     font-size: 16px;
     color: #7927E0;
 }
